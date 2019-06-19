6e3655d19330a508fa8ce3a65421b24402f571e1
Azeem Khalid <azeem@sofy.ai>
2019-06-19 13:11:23 +0500

    First Commit

---
+++ library/src/main/java/com/example/library/main.java
@@ -0,0 +1,15 @@
+package com.example.library;
+
+import android.util.Log;
+
+public class main {
+    String name;
+    String section;
+    String skill;
+
+    public static void getName()
+    {
+        Log.i("Azeem","Azeem");
+    }
+
+}
